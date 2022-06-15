c648f98de15cf0ac43b81340853b781a09d0e416
fhtwl <15927027721@163.com>
2022-06-15 17:12:22 +0800

    add Permission

--- src/Role/index.d.ts
+++ src/Role/index.d.ts
@@ -1,8 +1,25 @@
 declare namespace System {
+  /**
+   * 角色
+   */
   export interface Role {
     id: number;
     menuList: System.Menu[];
     parentId: number;
     name: string;
+  }
+  /**
+   * 角色权限
+   */
+  export interface Permission {
+    name: string;
+    roleId: number;
+    id: number;
+    menuType: System.MenuType;
+    show: number;
+    parentId: number;
+    serialNum: number;
+    permission: string;
+    actions: System.Action[];
   }
 }
